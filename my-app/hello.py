64db0e432128ed5aee075e4b116cce5283a0a91b
santii_ander <santiagoandermatten1@gmail.com>
2024-02-29 16:39:59 -0300

    fin del video de yt

--- my-app/hello.py
+++ my-app/hello.py
@@ -1,6 +1,9 @@
-from flask import Flask,render_template, url_for
+from flask import Flask,render_template, url_for, request
 
 app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY = "dev"
+)
 
 @app.add_template_filter
 def today(date):
@@ -17,7 +20,7 @@ def index():
     print(url_for("hello",name = "Santiago",age=27))
     print(url_for("code", code = 'print("Hola")'))
     name = "Santiago"
-    amigos = ["Agustina","Pedro","Leon"]
+    amigos = ["Agustina","Franco","Leon"]
     date = datetime.now()
     return render_template("index.html",
     name=name,
@@ -44,3 +47,35 @@ from markupsafe import escape
 def code(code):
     return f"<code>{escape(code)}</code>"
 
+#Crear form con wtform
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class RegisterForm(FlaskForm):
+    username = StringField("Nombre de usuario: ", validators=[DataRequired(),Length(min=4,max=15)])
+    password = PasswordField("Password: ",validators=[DataRequired(),Length(min=4,max=25)])
+    submit = SubmitField ("Registrar:")
+
+
+#Registrar Usuario
+@app.route("/auth/register", methods = ['GET','POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        return f"Nombre de usuario: {username}, Contraseña: {password}"
+
+   # form = RegisterForm()
+   # print(request.form)
+   # if request.method== "POST":
+   #     username = request.form["username"]
+   #     password = request.form["password"]
+   #     if len(username) >=4 and len(username) <=25 and len(password) >=6 and len(password) <=40:
+   #         return f"Nombre de usuario: {username}, Contraseña: {password}"
+   #     else:
+   #         error= "El nombre de usuario debe tener entre 4 y 25 caracteres y la contraseña entre 4 y 25"
+   #         return render_template("auth/register.html",form= form, error= error)
+    return render_template("auth/register.html",form= form)
